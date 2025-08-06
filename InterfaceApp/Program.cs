9dbf226aaaf01d85195ca9df9c89f376f9687ad9
Felix Kojo Otchere <fkotchere@st.ug.edu.gh>
2025-08-06 06:51:09 +0100

    Add Interfaces program

---
+++ InterfaceApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+// Interface - acts like a contract
+interface IMovable
+{
+    void Move();
+}
+
+// Car implements IMovable
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("The car drives forward smoothly.");
+    }
+}
+
+// Bicycle implements IMovable
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("The bicycle pedals forward steadily.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Objects that follow IMovable contract
+        IMovable myCar = new Car();
+        IMovable myBike = new Bicycle();
+
+        myCar.Move();   // Should say car drives forward
+        myBike.Move();  // Should say bicycle pedals forward
+    }
+}
